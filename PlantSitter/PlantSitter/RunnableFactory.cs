e59bcff8dbc72c28eb23f419d930f3d53241fcc0
Brian Flink <brifl@microsoft.com>
2016-11-14 21:29:52 -0800

    testing rpi3

--- PlantSitter/PlantSitter/RunnableFactory.cs
+++ PlantSitter/PlantSitter/RunnableFactory.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Threading.Tasks;
 using Windows.ApplicationModel.Background;
+using Windows.Devices.Gpio;
 
 namespace PlantSitter
 {
@@ -7,7 +9,45 @@ namespace PlantSitter
     {
         public IRunnable GetRunnable(IBackgroundTaskInstance taskInstance)
         {
-            throw new NotImplementedException();
+
+            return new TestLedRunnable(taskInstance);
+        }
+    }
+
+    internal class TestLedRunnable : IRunnable
+    {
+        private bool _isInitialized = false;
+
+        private GpioPin Red { get; set; }
+        private GpioPin Green { get; set; }
+        private GpioPin Blue { get; set; }
+
+        public TestLedRunnable(IBackgroundTaskInstance taskInstance)
+        {
+        }
+
+        public async Task Run()
+        {
+            await Init();
+
+            Green.Write(GpioPinValue.High);
+        }
+
+        private async Task Init()
+        {
+            if (!_isInitialized)
+            {
+                var gpio = await GpioController.GetDefaultAsync();
+
+                Red = gpio.OpenPin(2);
+                Red.SetDriveMode(GpioPinDriveMode.Output);
+                Green = gpio.OpenPin(3);
+                Green.SetDriveMode(GpioPinDriveMode.Output);
+                Blue = gpio.OpenPin(4);
+                Blue.SetDriveMode(GpioPinDriveMode.Output);
+
+                _isInitialized = true;
+            }
         }
     }
 }
